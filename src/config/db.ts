b6c71943accd375fdb933e4ec5ab28a8cff42964
dev-bernardofofg <dev.bernardofofg@gmail.com>
2025-05-23 17:45:11 -0300

    fix: db config url variable not match

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI!);
+    await mongoose.connect(process.env.MONGODB_URL!);
     console.log('MongoDB conectado');
   } catch (err) {
     console.error('Erro ao conectar com MongoDB:', err);
