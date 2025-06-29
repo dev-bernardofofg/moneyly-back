245d98d724aa68980cecf2cb2d2c853ac685f1c2
dev-bernardofofg <dev.bernardofofg@gmail.com>
2025-06-29 16:41:01 -0300

    feat: add categories route to the application
    Integrates a new route for category management, allowing users to access category-related functionalities. Updates the main router to include the CategoryRouter, enhancing the overall routing structure and improving code organization.

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import AuthRouters from "./routes/auth";
+import CategoryRouter from "./routes/category";
 import TransactionsRouters from "./routes/transaction";
 
-const router = Router()
+const router: Router = Router();
 
-router.use('/users', AuthRouters);
-router.use('/transactions', TransactionsRouters);
+router.use("/users", AuthRouters);
+router.use("/transactions", TransactionsRouters);
+router.use("/categories", CategoryRouter);
 
 export default router;
